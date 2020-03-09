23258b7c021349deb4ec256d0a468545af7253cd
RandyMejiaArias <rdmejia3@espe.edu.ec>
2020-03-09 18:34:51 -0500

    Tasks form completed

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-<?php
-    include("db.php")
-?>
+<!---- Connection to DB ---->
+<?php include("db.php") ?>
 
-<?php
-    include("includes/header.php")
-?>
-    
-<?php
-    include("includes/footer.php")
-?>
+<!---- HTML Header ---->
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="Description" rows="3" class="form-control" placeholder="Task description"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Save" class="btn btn-success btn-block" name="save_task">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!---- HTML Footer ---->
+<?php include("includes/footer.php") ?>
